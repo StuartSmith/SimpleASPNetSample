fcbd87394d4e1192d292bcd79c50cf86b6780d8f
Stuart Smith <ssmith_1975@yahoo.com>
2016-10-03 06:30:04 -0700

    Refactoring code so it is easier to Maintain made each html file load on startup

---
+++ src/SimpleASPNetSample/wwwroot/js/ServoStatus.js
@@ -0,0 +1,52 @@
+﻿////__________________________________________________
+/// Servo Status Result
+///____________________________________________________
+var ServoStatus = function (data, status) {
+    $("#ServoStatus").html(data);
+}
+
+var ServoStatusResult = function (data, status) {
+    var url = "/api/servo/statuses?=" + new Date().getTime();
+    aClient = new HttpClient();
+    aClient.get(url, ServoStatusResultComplete);
+}
+
+var ServoStatusResultComplete = function (data, status) {
+    $("#ServoChangeStatusResults").html(data);
+}
+
+  $(document).ready(function () {
+        //____________________________________________________________________________
+        //Servo Statuses and Position
+        //____________________________________________________________________________
+
+        $("#ButtonServoStatus").click(function () {
+            var url = "/api/servo/statuses?=" + new Date().getTime();
+            aClient = new HttpClient();
+            aClient.get(url, ServoStatus);
+        });
+
+        $("#ButtonServoStatus0").click(function () {
+            var WebServiceUrl = "/api/servo/statuses";
+            var servoData = { "servoStatus": "zeroDegrees", "description": "LaunchServo", "servoGPIO": 13 };
+            servoData.servoStatus = "zeroDegrees";
+            aClient = new HttpClient();
+            aClient.put(WebServiceUrl, servoData, ServoStatusResult);
+        });
+
+        $("#ButtonServoStatus90").click(function () {
+            var WebServiceUrl = "/api/servo/statuses";
+            var servoData = { "servoStatus": "NinetyDegrees", "description": "LaunchServo", "servoGPIO": 13 };
+            servoData.servoStatus = "NinetyDegrees";
+            aClient = new HttpClient();
+            aClient.put(WebServiceUrl, servoData, ServoStatusResult);
+        });
+
+        $("#ButtonServoStatus180").click(function () {
+            var WebServiceUrl = "/api/servo/statuses";
+            var servoData = { "servoStatus": "OneEightyDegrees", "description": "LaunchServo", "servoGPIO": 13 };
+            servoData.servoStatus = "OneEightyDegrees";
+            aClient = new HttpClient();
+            aClient.put(WebServiceUrl, servoData, ServoStatusResult);
+        });
+    });
---
+++ src/SimpleASPNetSample/wwwroot/js/UltraSonic.js
@@ -0,0 +1,20 @@
+﻿////__________________________________________________
+/// Ultra Sonic Result
+///____________________________________________________
+var UltraSonicStatus = function (data, status) {
+    $("#UltraSonicStatusResult").html(data);
+}
+
+$(document).ready(function () {
+
+    //____________________________________________________________________________
+    //Sonic sensor
+    //____________________________________________________________________________   
+
+    $("#ButtonUltraSonicStatus").click(function () {
+        var url = "/api/ultrasonic/statuses?=" + new Date().getTime();
+        aClient = new HttpClient();
+        aClient.get(url, UltraSonicStatus);
+    });
+
+});
