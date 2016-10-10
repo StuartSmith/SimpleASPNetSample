7ed3e644136f7ce1f4c1db481bf3552884875bc9
Stuart Smith <ssmith_1975@yahoo.com>
2016-10-10 08:30:24 -0700

    Updating code so I can have create ultrasonic rest controller

---
+++ src/SimpleASPNetSample/Models/UltraSonicSensorRunMeasurement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleASPNetSample.Models
+{
+    public class UltraSonicSensorRunMeasurement
+    {
+        public int SonicMeasurementId { get; set; }
+        public DateTime TimeOfMeasurment { get; set; }
+        public long MeasurementDistance { get; set; }
+        public int UltraSonicSensorRunId { get; set; }
+        public UltraSonicSensorRun Run { get; set; }
+    }
+}
