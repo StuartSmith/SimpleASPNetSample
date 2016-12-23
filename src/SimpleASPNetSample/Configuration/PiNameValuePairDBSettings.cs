3f655aedda58d1d25b727fbf5cd55244eaded300
Stuart Smith <ssmith_1975@yahoo.com>
2016-12-22 21:31:05 -0800

    Updating the PI value pair page to return values

--- src/SimpleASPNetSample/Configuration/PiNameValuePairDBSettings.cs
+++ src/SimpleASPNetSample/Configuration/PiNameValuePairDBSettings.cs
@@ -110,6 +110,7 @@ namespace SimpleASPNetSample.Configuration
                 {
                     PairToModify.Value = Value;
                     db.PiNameValuePairs.Update(PairToModify);
+                    db.SaveChanges();
                     return true;
                 }
                 else
--- src/SimpleASPNetSample/wwwroot/js/PiConfiguration.js
+++ src/SimpleASPNetSample/wwwroot/js/PiConfiguration.js
@@ -1,1 +1,92 @@
 ﻿
+////__________________________________________________
+/// Pi Configuration status Results
+///____________________________________________________
+var ButtonRefreshResult = function (data, status) {
+    
+
+    var configurationValues = JSON.parse(data);
+
+    UpdateCheckBox(configurationValues, "AllowSendingofData", "#tglAllowSendingofData");
+    UpdateCheckBox(configurationValues, "AllowSendingToastLightData", "#tglAllowSendingToastLightData");
+    UpdateCheckBox(configurationValues, "AllowSendingToastServoData", "#tglAllowSendingToastServoData");
+    UpdateCheckBox(configurationValues, "AllowSendingUltraSonicData", "#tglAllowSendingUltraSonicData");
+
+    UpdateTextBox(configurationValues, "AzureIOTConnectionString", "#AzureConnectinString");
+    UpdateTextBox(configurationValues, "ToastWebSendURL", "#AzureToastServiceURL");
+}
+
+var ButtonSaveResult = function (data, status) {
+   
+}
+
+//_______________________________________
+// Grab a checkbox from the html, retrieve
+// its value and update an array that will eventually
+// be sent back to the rest service
+// ControlName: is the name of the check box control on the HTML
+// ValueName: is the name of the value to save in the array
+//_______________________________________
+var UpdateArrarywithCheckedValue = function (arr, ControlName, ValueName)
+{
+    var checkedvalue = $(ControlName).prop('checked');
+    arr.push({ name: ValueName, value: (checkedvalue.toString()) });
+}
+//_______________________________________
+// Grab the value of a textbox control from the html, retrieve
+// its value and update an array that will eventually
+// be sent back to the rest service
+// ControlName: is the name of the check box control on the HTML
+// ValueName: is the name of the value to save in the array
+//_______________________________________
+var UpdateArrarywithTextBoxValue = function (arr, ControlName, ValueName) {
+    var checkedvalue = $(ControlName).prop('value');
+    arr.push({ name: ValueName, value: (checkedvalue.toString()) });
+}
+
+
+var UpdateCheckBox = function (configurationValues, ValueName, CheckBoxName) {
+    var valuePair = configurationValues.filter(function (configurationValues) { return configurationValues.name.toUpperCase() == ValueName.toUpperCase() });
+    if (valuePair.length > 0) {
+        if (valuePair[0].value.toUpperCase() == "true".toUpperCase())
+            $(CheckBoxName).prop("checked", true);
+        else
+            $(CheckBoxName).prop("checked", false);
+    }
+}
+
+var UpdateTextBox = function (configurationValues, ValueName, TextBoxName) {
+    var valuePair = configurationValues.filter(function (configurationValues) { return configurationValues.name.toUpperCase() == ValueName.toUpperCase() });
+   
+    if (valuePair.length > 0) {
+        $(TextBoxName).prop("value", valuePair[0].value);
+    }
+}
+
+$(document).ready(function () {
+   
+    //Click event to retrieve all light statuses
+    $("#ButtonConfigurationRefresh").click(function () {
+       
+        var url = "/api/configuration?=" + new Date().getTime();
+        aClient = new HttpClient();
+        aClient.get(url, ButtonRefreshResult);
+    });
+
+    $("#ButtonConfigurationSave").click(function () {
+        
+        var arr = [];
+
+        UpdateArrarywithCheckedValue(arr, "#tglAllowSendingofData", "AllowSendingofData");
+        UpdateArrarywithCheckedValue(arr, "#tglAllowSendingToastLightData", "AllowSendingToastLightData");
+        UpdateArrarywithCheckedValue(arr, "#tglAllowSendingToastServoData", "AllowSendingToastServoData");
+        UpdateArrarywithCheckedValue(arr, "#tglAllowSendingUltraSonicData", "AllowSendingUltraSonicData");
+
+        UpdateArrarywithTextBoxValue(arr, "#AzureConnectinString", "AzureIOTConnectionString");
+        UpdateArrarywithTextBoxValue(arr, "#AzureToastServiceURL", "ToastWebSendURL");
+
+        var WebServiceUrl = "/api/configuration";
+        var aClient = new HttpClient();
+        aClient.put(WebServiceUrl, arr, ButtonSaveResult);
+    });
+});
